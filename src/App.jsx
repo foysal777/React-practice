2518bf9867e7f97e4f02fed646b0f4a79bc8f31a
foysal777 <foysal.cse11@gmail.com>
2025-01-24 09:03:38 +0600

    add service components

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 
 import './App.css'
 import Home from './components/Home/Home'
+import Service from './components/Service/Service';
 
 function App() {
 
@@ -8,7 +9,11 @@ function App() {
     <>
       <Home></Home>
       <div><h2>Welcome to React Md. foysal hossain Munna</h2> </div>
+      <Service  name = 'Mamun'  age='20' ></Service>
+      <Service name = 'Sohel' age = '22'></Service>
+      <Service name = 'Rohan' age = '26'></Service>
       
+
 
     </>
   )
--- src/components/Service/Service.jsx
+++ src/components/Service/Service.jsx
@@ -1,12 +1,36 @@
+import React from "react"
+import './Service.css'
 
+const Service = (props) => {
 
+    console.log(props)
 
-const  Service=() => {
+    // function update() {
+
+    //     const btn = document.getElementById("change")
+    //     btn.innerText = "I am changed Now "
+    //     btn.style.color = 'green'
+    // }
 
     return (
+        <div>
+            {/* <h2 id="change" className="text">  I am from service component</h2>
+            <button onClick={update} className="btn"> Click me & Change </button>
+            <br /><br /> */}
 
-        <div>I am from service component</div>
+            <div className="card">
+                <p>Customer name : {props.name} </p>
+                <p>Age : {props.age} </p>
+                <p>Launch date </p>
+
+            </div> <br />
+
+
+
+        </div>
     )
 }
 
 export default Service
+
+
